dc4e23e710df96b1a3bac5553f4f3cb2da460730
Brett Ennis <brettennis4@gmail.com>
2023-07-10 00:30:14 -0400

    added untracked files

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+
+export default function Header() {
+    return (
+        <div style={{
+            backgroundColor: "var(--gray-dark)",
+            width: "100%",
+            height: "6rem",
+            padding: "1.5rem",
+            fontFamily: "Maloney",
+            fontSize: "3.3rem",
+        }}>
+            <div style={{
+                color: "var(--gray-light)",
+                fontWeight: "normal",
+                position: "absolute",
+            }}>
+                glassy
+            </div>
+            <div style={{
+                color: "var(--light-blue)",
+                fontWeight: "bold",
+            }}>
+                glassy
+            </div>
+        </div>
+    )
+}
